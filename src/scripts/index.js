bd4f0e6f0030bb69dca0d505e5577396f1150d2b
Mifthahuddin <mifthah.huddin24@gmail.com>
2023-10-23 00:17:36 +0800

    Add Es Lint and Setup

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,11 +1,12 @@
-import 'regenerator-runtime'; 
+import 'regenerator-runtime';
 import '../styles/main.css';
-import '@fortawesome/fontawesome-free/js/fontawesome'
-import '@fortawesome/fontawesome-free/js/solid'
-import '@fortawesome/fontawesome-free/js/regular'
-import '@fortawesome/fontawesome-free/js/brands'
+import '@fortawesome/fontawesome-free/js/fontawesome';
+import '@fortawesome/fontawesome-free/js/solid';
+import '@fortawesome/fontawesome-free/js/regular';
+import '@fortawesome/fontawesome-free/js/brands';
 
 import { toggleMenu } from './navbar';
 import { fetchAndDisplayRestaurants } from './app';
+
 const hamburger = document.querySelector('.hamburger');
 hamburger.addEventListener('click', toggleMenu);
