6fb423cca4d5f07f857239e77d88deedf5b82664
Mifthahuddin <mifthah.huddin24@gmail.com>
2023-10-28 02:38:34 +0800

    add webpack manifest

--- webpack.common.js
+++ webpack.common.js
@@ -1,4 +1,5 @@
 /* eslint-disable import/no-extraneous-dependencies */
+const WebpackPwaManifest = require('webpack-pwa-manifest');
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
@@ -37,6 +38,28 @@ module.exports = {
         {
           from: path.resolve(__dirname, 'src/public/'),
           to: path.resolve(__dirname, 'dist/'),
+        },
+      ],
+    }),
+    new WebpackPwaManifest({
+      name: 'My Progressive Web App',
+      short_name: 'MyPWA',
+      description: 'My awesome Progressive Web App!',
+      background_color: '#ffffff',
+      crossorigin: 'use-credentials', // can be null, use-credentials or anonymous
+      icons: [
+        {
+          src: path.resolve('src/assets/icon.png'),
+          sizes: [96, 128, 192, 256, 384, 512], // multiple sizes
+        },
+        {
+          src: path.resolve('src/assets/large-icon.png'),
+          size: '1024x1024', // you can also use the specifications pattern
+        },
+        {
+          src: path.resolve('src/assets/maskable-icon.png'),
+          size: '1024x1024',
+          purpose: 'maskable',
         },
       ],
     }),
